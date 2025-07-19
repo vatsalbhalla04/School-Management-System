fe8aafc5a667991390090819c51ec3a2c1dca7d8
Vatsal bhalla <118548880+vatsalbhalla04@users.noreply.github.com>
2025-07-19 18:45:03 +0530

    Updated the section.validation.js

--- Backend/src/validators/admin/section.validator.js
+++ Backend/src/validators/admin/section.validator.js
@@ -2,10 +2,9 @@ import z from "zod";
 
 const sectionValidation = z.object({
     SecName : z.string().min(1).max(1).optional(),
-    StdName: z.string().min(2).max(4), 
+    StdName: z.string().min(2).max(4).optional(), 
     classTeacherUsername: z.string().optional(), 
-    newSecName: z.string().min(1).max(1).optional(),
-    currentSecName: z.string().min(1).max(1).optional()
+    currentSecName: z.string().min(1).max(1).optional(),
 }); 
 
 export default sectionValidation;              
