83da4b0b19910ccc362cf7d1cd89b1964a84e53c
Vatsal bhalla <118548880+vatsalbhalla04@users.noreply.github.com>
2025-07-19 20:27:05 +0530

    Done with subject.route.js

--- Backend/src/Routes/adminRoutes/subAdminRoutes/subject.route.js
+++ Backend/src/Routes/adminRoutes/subAdminRoutes/subject.route.js
@@ -1,8 +1,9 @@
-import { Router } from "express";
-import { TryCatch } from "../../../middleware/error.js";
-import subjectRouteValidations from "../../../validators/admin/subject.validator.js";
-import ErrorHandler from "../../../utils/utility.js";
 import pkg from "@prisma/client";
+import { Router } from "express";
+import { success } from "zod";
+import { TryCatch } from "../../../middleware/error.js";
+import ErrorHandler from "../../../utils/utility.js";
+import subjectRouteValidations from "../../../validators/admin/subject.validator.js";
 
 const { PrismaClient } = pkg;
 
@@ -13,7 +14,6 @@ const subjectRoute = Router();
 subjectRoute.post(
   "/add-subject",
   TryCatch(async (req, res, next) => {
-
     const result = subjectRouteValidations.safeParse(req.body);
     if (!result.success)
       return next(new ErrorHandler("Validations Failed", 4040));
@@ -67,7 +67,7 @@ subjectRoute.post(
           select: {
             teacher: {
               select: {
-                firstname:true,
+                firstname: true,
                 lastname: true,
                 username: true,
               },
@@ -86,22 +86,208 @@ subjectRoute.post(
       success: true,
       message: `Subject ${SubjectName} created successfully under standard ${standard.StdName} and assigned to ${teacherUser.username}`,
       addSubject,
-    });    
+    });
   })
 );
 
 subjectRoute.put(
-  "/update-subject-details",
-  TryCatch(async (req, res, next) => {})
+  "/update-subject-details/:id",
+  TryCatch(async (req, res, next) => {
+    const result = subjectRouteValidations.safeParse(req.body);
+    if (!result.success)
+      return next(new ErrorHandler("Validations Failed", 404));
+
+    const id = Number(req.params.id);
+
+    const { newSubjectName, newStd, newFacultyAssigned } = result.data;
+
+    const subject = await prisma.subject.findUnique({
+      where: { id },
+    });
+
+    if (!subject) return next(new ErrorHandler("No Subject Found", 404));
+
+    const standard = await prisma.standard.findUnique({
+      where: {
+        StdName: newStd,
+      },
+    });
+
+    if (!standard) return next(new ErrorHandler("Standard not found", 404));
+
+    const user = await prisma.user.findUnique({
+      where: {
+        username: newFacultyAssigned,
+      },
+    });
+
+    if (!user || user.role !== "TEACHER")
+      return next(new ErrorHandler("Teacher Not Found"));
+
+    const teacher = await prisma.teacher.findUnique({
+      where: { teacherId: user.id },
+    });
+
+    if (!teacher)
+      return next(new ErrorHandler("No Teacher Profile found", 404));
+
+    if (
+      subject.name === newSubjectName &&
+      standard.StdName === newStd &&
+      teacher.username === newFacultyAssigned
+    )
+      return next(new ErrorHandler("Nothing to change", 200));
+
+    const updateSubjectDetails = await prisma.subject.update({
+      where: {
+        id,
+      },
+      data: {
+        name: newSubjectName,
+        standard: { connect: { id: standard.id } },
+        teacher: { connect: { id: teacher.id } },
+      },
+      select: {
+        name: true,
+        standard: {
+          select: {
+            StdName: true,
+          },
+        },
+        teacher: {
+          select: {
+            teacher: {
+              select: {
+                id: true,
+                firstname: true,
+                lastname: true,
+                username: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Details Updated Succesfully",
+      Update_Subject_Details: updateSubjectDetails,
+    });
+  })
 );
 
 subjectRoute.delete(
-  "/delet-subject",
-  TryCatch(async (req, res, next) => {})
+  "/delete-subject/:id",
+  TryCatch(async (req, res, next) => {
+    const id = Number(req.params.id);
+
+    const subject = await prisma.subject.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!subject) return next(new ErrorHandler("No Subject Found", 404));
+
+    const deletedSubject = await prisma.subject.delete({
+      where: { id },
+      select: {
+        id: true,
+        name: true,
+        teacher: {
+          select: {
+            teacher: {
+              select: {
+                firstname: true,
+                lastname: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Subject Deleted Sucesfully",
+      Deleted_Subject_Details: deletedSubject,
+    });
+  })
 );
 
 subjectRoute.get(
   "/all-subjects",
-  TryCatch(async (req, res, next) => {})
+  TryCatch(async (req, res, next) => {
+    const subjectDetails = await prisma.subject.findMany({
+      select: {
+        name: true,
+        teacher: {
+          select: {
+            teacher: {
+              select: {
+                firstname: true,
+                lastname: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (subjectDetails.length === 0) {
+      res.status(200).json({
+        success: true,
+        message: "No Subjects Added",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: `Total subjects are ${subjectDetails.length}`,
+      data: subjectDetails,
+    });
+  })
 );
+
+subjectRoute.get(
+  "/subject-details/:id",
+  TryCatch(async (req, res, next) => {
+    const id = Number(req.params.id);
+
+    const subject = await prisma.subject.findUnique({
+      where: { id },
+    });
+
+    if (!subject) return next(new ErrorHandler("No Subject Found", 404));
+
+    const subjectDetails = await prisma.subject.findFirst({
+      select: {
+        id: true,
+        name: true,
+        teacher: {
+          select: {
+            teacher: {
+              select: {
+                id: true,
+                firstname: true,
+                lastname: true,
+                username: true,
+                phoneNumber: true,
+                email: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      message: `Fetched Single Subjects Details Succesfully`,
+      data: subjectDetails,
+    });
+  })
+);
+
 export default subjectRoute;
